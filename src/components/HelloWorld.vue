226e44b989f2d117e3989f9a85a035f34da9a620
Sergio Faya <sergio.faya@personio.de>
2022-01-02 16:58:21 +0100

    Add loading spinner

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
     <h1>{{ msg }}</h1>
+    <div class="pt-3 pb-3">
+      <button
+        class="btn btn-primary"
+        @click="setSpinner"
+      >Loading</button>
+    </div>
+
   </div>
 </template>
 
@@ -14,6 +21,14 @@ import { Options, Vue } from 'vue-class-component';
 })
 export default class HelloWorld extends Vue {
   msg!: string
+
+  setSpinner(): void {
+    const loader = this.$loading.show();
+
+    setTimeout(() => {
+      loader.hide();
+    }, 3000);
+  }
 }
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
+// bootstrap css
 import 'bootstrap/dist/css/bootstrap.css';
+// vue loading overlay config
+import VueLoading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+
 import { createApp } from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 
 createApp(App)
+  .use(VueLoading)
   .use(router)
   .mount('#app');
 
